26de0a0bae8d2ff917d7feb6c6949c9c5c923b83
Michael Weinberger <m@gmail.com>
2022-11-02 14:19:05 -0400

    updated defend registration usecase with code changes to create better logging messages

--- pangea-defend-user-registration/integration/integration.action.js
+++ pangea-defend-user-registration/integration/integration.action.js
@@ -1,64 +1,82 @@
-exports.onExecutePreUserRegistration = async(event, api) => {
-	const Pangea = require('node-pangea');
-	const domain = "aws.us.pangea.cloud";
-	const token = event.secrets.TOKEN;
-	const configId = event.configuration.CONFIGID;
-	const config = new Pangea.PangeaConfig({
-			domain: domain,
-			configId: configId
-	});
-	const audit = new Pangea.AuditService(token, config);
-	const embargo = new Pangea.EmbargoService(token, config);
-	const domainIntel = new Pangea.DomainIntelService(token, config);
 
-	const data = {
-			actor: event.user.email,
-			action: "Registration",
-			status: "success",
-			message: "",
-	};
-	const ip = event.request.ip;
-	console.log("Checking Embargo IP : '%s'", ip);
-	const ebmargo_response = await embargo.ipCheck(ip);
-	console.log("Response: ", ebmargo_response.result);
-	console.log("Checking Domain : '%s'", domain);
-	const options = {
-			provider: "domaintools",
-			verbose: true,
-			raw: true
-	};
-	const domain_response = await domainIntel.lookup(event.user.email.split("@")[1], options);
-	console.log(domain_response.result);
+exports.onExecutePreUserRegistration = async (event, api) => {
+	  const Pangea = require('node-pangea');
+	  const domain = "aws.us.pangea.cloud";
+	  const token = event.secrets.token;
+	  const configId = event.configuration.configId;
+	  const config = new Pangea.PangeaConfig({ domain: domain, configId: configId });
+	  const audit = new Pangea.AuditService(token, config);
+	  const embargo = new Pangea.EmbargoService(token, config);
+	  const domainIntel = new Pangea.DomainIntelService(token, config);
 
-	if (ebmargo_response.result.count > 0) {
+	  const ip = event.request.ip;
+	  const check_domain = event.user.email.split("@")[1];
+	  const options = { provider: "domaintools", verbose: true, raw: true };
+	  
+	  let context = {
+		      "connection":event.connection,
+		      "request":event.request,
+		      "user":event.user
+		      };
+	  let data = {
+		      "actor": event.user.email,
+		      "action": "New Account Registration Defense",
+		      "target": event.request.hostname,
+		      "new": context,
+		      "source": check_domain,
+		      "message": ""
+		      };
 
-			const LOCALIZED_MESSAGES = {
-						en: 'You are not allowed to register from an embargoed country.',
-						es: 'No tienes permitido registrarte.'
-					};
+	  var embargo_response;
+	  var domain_response;
 
-			const userMessage = LOCALIZED_MESSAGES[event.request.language] || LOCALIZED_MESSAGES['en'];
-			api.access.deny('no_signups_from_embargo', userMessage);
+	  try{
+		      //console.log("Checking Embargo IP : '%s'", ip);
+		      domain_response = await domainIntel.lookup(check_domain, options);
+		      data.new['domain_response'] = domain_response.gotResponse.body;
+		      //console.log("Response: ", ebmargo_response.gotResponse.body);
+		    } catch(error){
+			        domain_response = {"status":"Failed", "summary":error};
+			      };
 
-			data.status = "failed"
-				data.message = ebmargo_response.result
-				const logResponse = await audit.log(data);
-	} else if (domain_response.result.raw_data.response.risk_score > 70) {
+	  try{
+		      //console.log("Checking Embargo IP : '%s'", ip);
+		      embargo_response = await embargo.ipCheck(ip);
+		      data.new['embargo_response'] = embargo_response.gotResponse.body;
+		      //console.log("Response: ", ebmargo_response.gotResponse.body);
+		    } catch(error){
+			        embargo_response = {"status":"Failed", "summary":error};
+			      };
+	  
+	  
+	  if (embargo_response.status == "Success" && embargo_response.result.count == 0){
+		      data["status"] = "Success";
+		      data["message"] += " Passed Embargo Check,";
+		    };
+	  if (domain_response.status == "Success" && domain_response.result.raw_data.response.risk_score < 70){
+		      data["status"] = "Success";
+		      data["message"] += " Passed Domain Check,";
+		    };
+	  if (embargo_response.result.count > 0 ||  embargo_response.status != "Success" || domain_response.status != "Success" || domain_response.result.raw_data.response.risk_score > 70){
+		      const LOCALIZED_MESSAGES = {
+			            en: 'You are not allowed to register.',
+			            es: 'No tienes permitido registrarte.'
+			          };
 
-			const LOCALIZED_MESSAGES = {
-						en: 'You are not allowed to register with a suspicious email domain.',
-						es: 'No tienes permitido registrarte.'
-					};
-
-			const userMessage = LOCALIZED_MESSAGES[event.request.language] || LOCALIZED_MESSAGES['en'];
-			api.access.deny('no_signups_from_sus_domain', userMessage);
-
-			data.status = "failed"
-				data.message = domain_response.result
-				const logResponse = await audit.log(data);
-	} else {
-
-			data.message = "User successfully registered"
-				const logResponse = await audit.log(data);
-	};
+		      const userMessage = LOCALIZED_MESSAGES[event.request.language] || LOCALIZED_MESSAGES['en'];
+		      api.access.deny('no_signups_from_failed_check', userMessage);
+		      data["status"] = "Failed";
+		      if (embargo_response.result.count > 0 ||  embargo_response.status != "Success"){
+			              data["message"] += " Failed Embargo Check - " + embargo_response.summary + ",";
+			          };
+		      if (domain_response.result.raw_data.response.risk_score > 70){
+			            domain_response.summary = "Domain was determined to be suspicious with a score of " + domain_response.result.raw_data.response.risk_score;
+			            data["message"] += " Failed Domain Check - " + domain_response.summary + ",";
+			          }
+		      else if (domain_response.status != "Success"){
+			            data["message"] += " Failed Domain Check - " + domain_response.summary + ",";
+			          };
+		    };
+	 
+	  const logResponse = await audit.log(data);
 };
